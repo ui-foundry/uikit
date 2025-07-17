35382544c9b68ac548f06192edca93afb2c981a0
Ramki A <kvraamkey@gmail.com>
2025-07-17 18:33:45 +0530

    style: core style initited

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "@stencil/core";
 
 export const config: Config = {
-	namespace: "core",
-	extras: {
-		enableImportInjection: true,
-	},
-	outputTargets: [
-		{
-			type: "dist",
-			esmLoaderPath: "../loader",
-		},
-	],
+  namespace: "core",
+  extras: {
+    enableImportInjection: true,
+  },
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader",
+    },
+  ],
 };
