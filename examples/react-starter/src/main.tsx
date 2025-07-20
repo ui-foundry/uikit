85d06387c45e5a9b0398a4125d249357d9ef4d35
Ramki A <kvraamkey@gmail.com>
2025-07-20 10:11:38 +0530

    docs: react example added

---
+++ examples/react-starter/src/main.tsx
@@ -0,0 +1,16 @@
+import { UkAlert } from "@ui-foundry/uikit-react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "@ui-foundry/uikit-react/styles.css";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<UkAlert heading="Alert Heading">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsam
+			nesciunt voluptates iusto nemo, eos doloribus aliquid praesentium
+			dolorem! Laboriosam natus cupiditate fuga iusto maxime quis
+			excepturi, deleniti quo similique!
+		</UkAlert>
+	</StrictMode>,
+);
