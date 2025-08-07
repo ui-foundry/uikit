8763846e16a28b503bc5a62899e272a6caf1e0b0
Ramki A <kvraamkey@gmail.com>
2025-08-07 16:11:37 +0530

    feat: vue component updated

--- packages/vue/src/components.ts
+++ packages/vue/src/components.ts
@@ -7,4 +7,6 @@ import type { JSX } from "@uifoundry/uikit";
 
 export const UiAvatar: StencilVueComponent<JSX.UiAvatar> = /*@__PURE__*/ defineContainer<JSX.UiAvatar>("ui-avatar", undefined, ["image", "alt", "initials", "radius", "size"]);
 
-export const UiFlex: StencilVueComponent<JSX.UiFlex> = /*@__PURE__*/ defineContainer<JSX.UiFlex>("ui-flex", undefined, ["gap"]);
+export const UiFlex: StencilVueComponent<JSX.UiFlex> = /*@__PURE__*/ defineContainer<JSX.UiFlex>("ui-flex", undefined, ["gap", "direction", "wrap", "align", "justify"]);
+
+export const UiSpinner: StencilVueComponent<JSX.UiSpinner> = /*@__PURE__*/ defineContainer<JSX.UiSpinner>("ui-spinner", undefined, ["size"]);
