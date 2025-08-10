93b715f8ad149980a7ed590f49cd0af9ae7fad62
Ramki A <kvraamkey@gmail.com>
2025-08-10 23:05:47 +0530

    docs: badge component doc updated

--- docs/.vitepress/theme/sidebar/components.ts
+++ docs/.vitepress/theme/sidebar/components.ts
@@ -1,13 +1,29 @@
+import fs from "node:fs";
+import path from "node:path";
 import type { DefaultTheme } from "vitepress";
+
+function getComponentSidebarItems(): DefaultTheme.SidebarItem[] {
+	const componentsDir = path.resolve(__dirname, "../../../src/components");
+
+	// Read files from components folder
+	const files = fs.readdirSync(componentsDir);
+
+	// Map them into sidebar items
+	return files
+		.filter((file) => file.endsWith(".md")) // only .md files
+		.map((file) => {
+			const name = path.basename(file, ".md");
+			return {
+				text: name.charAt(0).toUpperCase() + name.slice(1),
+				link: `/uikit/components/${name}`,
+			};
+		})
+		.sort((a, b) => a.text.localeCompare(b.text));
+}
 
 export const SideBarComponents: DefaultTheme.SidebarItem[] = [
 	{
 		text: "Components",
-		items: [
-			{ text: "Alert", link: "/uikit/components/alert" },
-			{ text: "Avatar", link: "/uikit/components/avatar" },
-			{ text: "Icon", link: "/uikit/components/icon" },
-			{ text: "Spinner", link: "/uikit/components/spinner" },
-		],
+		items: getComponentSidebarItems(),
 	},
 ];
---
+++ docs/src/components/badge.md
@@ -0,0 +1,19 @@
+---
+title: Badge
+description: Display badge, pill or tag
+---
+
+# Badge <span class="text-size-3.5">`<ui-badge/>`</span>
+
+Badges are used to draw attention and display statuses or counts.
+
+The Badge component serves the purpose of presenting information to the user. It can display some quick info and can be customized to draw the user's attention by modifying its color, size and other properties.
+
+## Usage
+<ui-flex gap="10">
+	<ui-badge radius="xs">Badge</ui-badge>
+    <ui-badge size="20" circle>8</ui-badge>
+    <ui-badge radius="pill">99</ui-badge>
+    <ui-badge radius="sm">999</ui-badge>
+    <ui-badge>99+</ui-badge>
+</ui-flex>
